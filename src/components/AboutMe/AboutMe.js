e04d2ed4c1efc5cff5649b8f61b56c584b4c44a3
Akim Minko <akim.minko@yandex.ru>
2023-08-26 10:39:06 +0300

    feat: AboutMe

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,7 +1,34 @@
-import "./AboutMe.css"
+import "./AboutMe.css";
+import photo from "../../images/photo.jpg";
 
 function AboutMe() {
-  return 
+  return (
+    <section className="student content__section">
+      <h2 className="content__title">Студент</h2>
+      <article className="about-me">
+        <div className="about-me__container">
+          <div className="about-me__info">
+            <h3 className="about-me__title">Аким</h3>
+            <p className="about-me__subtitle">Фронтенд-разработчик, 23 года</p>
+            <p className="about-me__description">
+              Я живу в Ростове-на-Дону, закончил факультет ИТУ. У меня есть кот Степан и кошка Молли. Увлекаюсь футболом. Люблю слушать музыку.
+              Работаю системным администратором. Стремлюсь к тому, чтобы полностью перейти в сферу программирования.
+            </p>
+          </div>
+          <ul>
+            <li className="about-me__contact">
+              <a className="about-me__link" href="https://github.com/Akim262000">
+                Github
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="about-me__photo-container">
+          <img className="about-me__photo" src={photo} alt="Фото студента" />
+        </div>
+      </article>
+    </section>
+  );
 }
 
 export default AboutMe;
