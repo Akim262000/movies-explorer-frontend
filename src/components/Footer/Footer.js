9968790588fa0d9b76a1156444a853a992f6da85
Akim Minko <akim.minko@yandex.ru>
2023-08-26 12:18:50 +0300

    feat: Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,18 @@
-import "./Footer.css"
+import "./Footer.css";
 
 function Footer() {
-  return 
+  return (
+    <footer className="footer">
+      <h4 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h4>
+      <div className="footer__container">
+        <p className="footer__year"> &copy; 2023</p>
+        <ul className="footer__links">
+          <li className="footer__item"><a href="https://practicum.yandex.ru" target='_blank' rel='noopener noreferrer'>Яндекс.Практикум</a></li>
+          <li className="footer__item"><a href="https://github.com/Akim262000" target='_blank' rel='noopener noreferrer'>Github</a></li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
