a6eeb6b693c3b4bb8788ccbd16ac73012cd12e05
Akim Minko <akim.minko@yandex.ru>
2023-08-30 12:27:24 +0300

    refactor: button save/delete movies

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -3,16 +3,16 @@ import "./MoviesCard.css"
 function MoviesCard(props) {
   return (
     <section className="movies-card">
-      <div className="movies-card__container">
-        <img className="movies-card-image" src={props.card.image} alt="Фильм"></img>
+        <img className="movies-card__image" src={props.card.image} alt="Фильм"></img>
         <div className="movies-card__info">
             <h3 className="movies-card__title">{props.card.nameRu}</h3>
             <p className="movies-card__duration">1ч 17м</p>
         </div>
-        <button className={`movies-card__button 
-        ${props.savedPage ? 'movies-card__button-delete' : 'movies-card__button-save'}
-        ${props.card.owner === 1 && !props.savedPage ? 'movies-card__button-save_active' : null}`} type="button"></button>
-      </div>
+        {props.card.owner === 1 && !props.savedPage && <button className="movies-card__button-save" />}
+        {props.savedPage ? (
+          <button className="movies-card__button-delete" />) 
+          : (<button className="movies-card__button">Сохранить</button>)
+        }
     </section>
   )
 }
